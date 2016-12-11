b13d2b60311fdb275bb7294face096645ed8d849
Alexander Trauzzi <atrauzzi@gmail.com>
2016-12-11 16:26:03 -0600

    Is work?

--- Praxis/publish.sh
+++ Praxis/publish.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ]; then
   echo "This is a new release, yay!  Pushing nuget package."
-  docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push Praxis.$TRAVIS_TAG.nupkg $NUGET_KEY
+  docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push Praxis.$TRAVIS_TAG.nupkg $NUGET_API_KEY
 else
   echo "Not a release, skipping nuget publish."
 fi
