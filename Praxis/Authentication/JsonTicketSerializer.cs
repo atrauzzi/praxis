bcb22fb031f1286aa608b41d7dfc0ce644261907
Alexander Trauzzi <atrauzzi@gmail.com>
2016-12-18 10:46:18 -0600

    Pushing save.

--- Praxis/Authentication/JsonTicketSerializer.cs
+++ Praxis/Authentication/JsonTicketSerializer.cs
@@ -99,10 +99,13 @@ namespace Praxis.Authentication
 
         private AuthenticationProperties DeserializeProperties(JToken data)
         {
-            return new AuthenticationProperties(data.SelectToken("properties")?.Value<JObject>()?.Properties()?.ToDictionary(
-                propertyJson => propertyJson.Name,
-                propertyJson => propertyJson.Value.Value<string>()
-            ));
+            return new AuthenticationProperties(data
+                .SelectToken("properties")?
+                .Value<JObject>()?
+                .Properties()?
+                .ToDictionary(
+                    propertyJson => propertyJson.Name,
+                    propertyJson => propertyJson.Value.Value<string>()));
         }
     }
 }
