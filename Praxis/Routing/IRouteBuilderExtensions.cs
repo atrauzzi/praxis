3b64b9849ffb23a3ab4c1330616fc758a75099df
Alexander Trauzzi <atrauzzi@gmail.com>
2016-12-19 09:23:15 -0600

    More routing sugar.

--- Praxis/Routing/IRouteBuilderExtensions.cs
+++ Praxis/Routing/IRouteBuilderExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Constraints;
@@ -7,6 +9,33 @@ namespace Praxis.Routing
 {
     public static class IRouteBuilderExtensions
     {
+        public static IRouteBuilder Add(
+            this IRouteBuilder routes,
+            Route route,
+            string controller,
+            string action
+        )
+        {
+            if (route.Method == null)
+            {
+                routes.Any(route.Path, controller, action, route.Name, route.ContentType);
+            }
+            else if(route.Method == HttpMethod.Get)
+            {
+                routes.Get(route.Path, controller, action, route.Name, route.ContentType);
+            }
+            else if (route.Method == HttpMethod.Post)
+            {
+                routes.Post(route.Path, controller, action, route.Name, route.ContentType);
+            }
+            else
+            {
+                throw new NotImplementedException("That HTTP verb is currently not supported.");
+            }
+
+            return routes;
+        }
+
         public static IRouteBuilder Any(
             this IRouteBuilder routes,
             string path,
---
+++ Praxis/Routing/Route.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+
+namespace Praxis.Routing
+{
+    public struct Route
+    {
+        public string Name { get; }
+
+        public string Path { get; }
+
+        public HttpMethod Method { get; }
+
+        public string ContentType { get; }
+
+        public Route(
+            HttpMethod method,
+            string path,
+            string name, 
+            string contentType = null
+        )
+        {
+            Method = method;
+            Path = path;
+            Name = name;
+            ContentType = contentType;
+        }
+
+        public static Route Any(string path, string name, string contentType = null) 
+            => new Route(null, path, name, contentType);
+
+        public static Route Get(string path, string name, string contentType = null)
+            => new Route(HttpMethod.Get, path, name, contentType);
+
+        public static Route Post(string path, string name, string contentType = null)
+            => new Route(HttpMethod.Post, path, name, contentType);
+
+    }
+}
