0642940761533b8a469ab8240a6bc39e2d8a0b66
Alexander Trauzzi <atrauzzi@gmail.com>
2017-01-14 12:40:44 -0600

    Very odd.

--- Praxis/publish.sh
+++ Praxis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PACK_COMMAND="docker-compose run --rm praxis-cli dotnet pack --configuration Release"
-PUSH_COMMAND_TEMPLATE="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Release/Praxis*.nupkg NUGET_API_KEY"
+PUSH_COMMAND_TEMPLATE="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Release/Praxis*.nupkg NUGET_API_KEY -Source https://www.nuget.org -Verbosity detailed"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 
