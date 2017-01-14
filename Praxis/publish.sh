925f2b4ec39af65f8c44a2a62ae79313e3bc7d87
Alexander Trauzzi <atrauzzi@gmail.com>
2017-01-14 12:19:21 -0600

    Try V3 maybe?

--- Praxis/publish.sh
+++ Praxis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PACK_COMMAND="docker-compose run --rm praxis-cli dotnet pack --configuration Release"
-PUSH_COMMAND_TEMPLATE="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Release/Praxis*.nupkg NUGET_API_KEY -Source https://www.nuget.org/api/v2/package"
+PUSH_COMMAND_TEMPLATE="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Release/Praxis*.nupkg NUGET_API_KEY -Source https://api.nuget.org/v3/index.json"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 
