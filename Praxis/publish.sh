ba1938e67eb43633db5959bd6932ec0495681e06
Alexander Trauzzi <atrauzzi@gmail.com>
2017-01-14 10:50:20 -0600

    Maybe this can work?

--- Praxis/publish.sh
+++ Praxis/publish.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ ! -z "$TRAVIS_TAG" ]; then
+PACK_COMMAND="docker-compose run --rm praxis-cli dotnet pack --include-source --configuration Release"
+PUSH_COMMAND="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Debug/Praxis*.nupkg $NUGET_API_KEY -Source nuget.org"
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
 
   if [ -z "$NUGET_API_KEY" ]; then
     echo "Missing nuget API key, unable to release."
   else
     echo "This is a new release, yay!  Publishing nuget package."
-	docker-compose run --rm praxis-cli dotnet pack --include-source --configuration Release --version-suffix 
-    docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Debug/Praxis.$TRAVIS_TAG.nupkg $NUGET_API_KEY -Source nuget.org
+	$PACK_COMMAND
+    $PUSH_COMMAND
   fi
 
 else
   echo "Not a release, skipping nuget publish."
+  echo "Commands run would be:"
+  echo $PACK_COMMAND
+  echo $PUSH_COMMAND
 fi
