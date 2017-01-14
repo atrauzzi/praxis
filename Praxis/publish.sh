3cab0f8303b9b1e6cd97e53b42d81d6695b7a867
Alexander Trauzzi <atrauzzi@gmail.com>
2017-01-14 11:11:33 -0600

    Template the API key.

--- Praxis/publish.sh
+++ Praxis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PACK_COMMAND="docker-compose run --rm praxis-cli dotnet pack --include-source --configuration Release"
-PUSH_COMMAND="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Debug/Praxis*.nupkg $NUGET_API_KEY -Source nuget.org"
+PUSH_COMMAND_TEMPLATE="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Debug/Praxis*.nupkg NUGET_API_KEY -Source nuget.org"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 
@@ -9,6 +9,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Missing nuget API key, unable to release."
   else
     echo "This is a new release, yay!  Publishing nuget package."
+	PUSH_COMMAND="${PUSH_COMMAND_TEMPLATE/NUGET_API_KEY/$NUGET_API_KEY}"
 	$PACK_COMMAND
     $PUSH_COMMAND
   fi
@@ -17,5 +18,5 @@ else
   echo "Not a release, skipping nuget publish."
   echo "Commands run would be:"
   echo $PACK_COMMAND
-  echo $PUSH_COMMAND
+  echo $PUSH_COMMAND_TEMPLATE
 fi
