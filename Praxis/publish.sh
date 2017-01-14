dce75e655605ca2e0f0bc3feddd9eb0cfcabf7bf
Alexander Trauzzi <atrauzzi@gmail.com>
2017-01-14 16:28:26 -0600

    Build straightened out.

--- Praxis/publish.sh
+++ Praxis/publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PACK_COMMAND="docker-compose run --rm praxis-cli dotnet pack --configuration Release"
-PUSH_COMMAND_TEMPLATE="docker-compose run --rm praxis-cli mono /usr/local/bin/nuget push ./bin/Release/Praxis*.nupkg NUGET_API_KEY -Source https://www.nuget.org -Verbosity detailed"
+PACK_COMMAND="dotnet pack --configuration Release"
+PUSH_COMMAND_TEMPLATE="mono /usr/local/bin/nuget push ./bin/Release/Praxis*.nupkg -ApiKey NUGET_API_KEY -Source https://www.nuget.org"
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$BRANCH" == "master" ]; then
 
   if [ -z "$NUGET_API_KEY" ]; then
     echo "Missing nuget API key, unable to release."
